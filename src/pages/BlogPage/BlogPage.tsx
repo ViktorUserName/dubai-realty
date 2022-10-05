6777ea051785ea993703f0227005d1417ea9622c
Roman <100953835+Chezy13@users.noreply.github.com>
2022-10-05 15:22:44 +0300

    start dev blog page

---
+++ src/pages/BlogPage/BlogPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const BlogPage = () => {
+    return (
+        <section>
+            <div className='wrapper'>
+                
+            </div>
+        </section>
+    );
+};
+
+export default BlogPage;
