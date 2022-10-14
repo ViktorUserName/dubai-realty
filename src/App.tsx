423b910c836e02d96b8aeb152cc4e928d2465711
Roman <100953835+Chezy13@users.noreply.github.com>
2022-10-14 13:55:24 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,23 @@ import ContactUs from './components/ContactUs/ContactUs';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import BlogPage from './pages/BlogPage/BlogPage';
+import CategoryPage from './pages/CategoryPage/CategoryPage';
 import ContactPage from './pages/ContactPage/ContactPage';
 import MainPage from './pages/MainPage/MainPage';
-import data from "./SharedLogic/BlogData.json";
+import ServicesPage from './pages/ServicesPage/ServicesPage';
+// import data from "./SharedLogic/BlogData.json";
 
 function App() {
 
-  const {results: posts} = data;
+  // const {results: posts} = data;
+
 
   return (
     <>
       <Header/>
-       {/* <BlogPage cardConfig={posts}/> */}
-       <ContactPage/>
+      {/* <BlogPage cardConfig={posts}/> */}
+      <ContactPage/>
       {/* <ContactUs/> */}
-      <MainPage/>
       <Footer/>
     </>
   );
