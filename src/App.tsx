8a16e66716d1b0432b9597b33d4613b97f822dba
ViktorUserName <vityachishevich@gmail.com>
2022-10-14 08:10:52 +0300

    up for pull

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
 import './App.scss';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import BlogPage from './pages/BlogPage/BlogPage';
+import CategoryPage from './pages/CategoryPage/CategoryPage';
+import ContactPage from './pages/ContactPage/ContactPage';
 import MainPage from './pages/MainPage/MainPage';
-
+import ServicesPage from './pages/ServicesPage/ServicesPage';
+// import data from "./SharedLogic/BlogData.json";
 
 function App() {
+
+  // const {results: posts} = data;
+
+
   return (
     <>
       <Header/>
-        <MainPage/>
+      <MainPage/>
       <Footer/>
     </>
   );
