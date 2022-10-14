1cf0a5e6f03bd3cf82b2ffb027e80d51b91bd2c2
Roman <100953835+Chezy13@users.noreply.github.com>
2022-10-14 13:59:53 +0300

    upp

--- src/App.tsx
+++ src/App.tsx
@@ -17,9 +17,7 @@ function App() {
   return (
     <>
       <Header/>
-      {/* <BlogPage cardConfig={posts}/> */}
-      <ContactPage/>
-      {/* <ContactUs/> */}
+      <MainPage/>
       <Footer/>
     </>
   );
