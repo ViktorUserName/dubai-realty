1fad1ad94f1ae9169fa6f487fe0abe59a9837b6b
ViktorUserName <vityachishevich@gmail.com>
2022-10-18 13:38:21 +0300

    update adoptive

--- src/pages/MainPage/blocks/Latest/Latest.tsx
+++ src/pages/MainPage/blocks/Latest/Latest.tsx
@@ -1,33 +1,46 @@
 import React, {useState} from 'react';
 import s from './Latest.module.scss'
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper';
+import { Navigation, Pagination, Grid } from 'swiper';
 import 'swiper/scss';
 import 'swiper/scss/navigation';
 import 'swiper/scss/pagination';
+import 'swiper/scss/grid';
 import './sliderLatest.scss'
 import latestCard1 from '../../../../img/latestCard1.png'
 import latestCard2 from '../../../../img/latestCard2.png'
 import latestCard3 from '../../../../img/latestCard3.png'
 
+// const [isShown, setIsShown] = useState(false);
 
+// return (
+//   <div className="App">
+//     <button
+//       onMouseEnter={() => setIsShown(true)}
+//       onMouseLeave={() => setIsShown(false)}>
 const LatestCard: React.FC = () => {
-    const [isLoading, setIsLoading] = useState(false);
+    const [isShown, setIsShown] = useState(false);
 
-    const toggleIsLoading = () => {
-      setIsLoading(current => !current);
-    };
+    // const toggleIsLoading = () => {
+    //   setIsLoading(current => !current);
+    // };
 
     return (
-        <div className={isLoading ? s.latestCardBack : s.latestCard} onClick={toggleIsLoading}>
+        // <div className={isLoading ? s.latestCardBack : s.latestCard} onClick={toggleIsLoading}>
+
+        <div className={isShown ? s.latestCardBack : s.latestCard}
+        onMouseEnter={() => setIsShown(true)}
+        onMouseLeave={() => setIsShown(false)}>
             <img src={latestCard1} className={s.latestCardImg} alt="" />
             <h2 className={s.latestCardText}>Villas</h2>
-            {isLoading &&
-             <div className={s.latestCardBackText}>
+            {isShown &&
+            //  <div className={s.latestCardBackText}>
+            <>
                 <h2 className={s.latestCardBackTextH2}>Dubai</h2>
                 <p className={s.latestCardBackTextP}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequat molestie integer aliquam consectetur. Faucibus vitae dui massa tellus magna sit.</p>
                 <button className={s.latestCardBackTextButton}>See project   {`>`}</button>
-             </div>
+            </>
+            // {/* //  </div> */}
             }
         </div>
     )
@@ -49,17 +62,24 @@ const Latest: React.FC = () => {
                     </div>
                     <Swiper
                         className={s.latestSlider}
-                        modules={[Pagination,]}
-                        spaceBetween={30}
-                        slidesPerView={4}
+                        modules={[Pagination, Grid]}
+                            
+                        slidesPerView={1}
                         slidesPerGroup={4}
                         pagination={{ 
                             clickable: true,
                             modifierClass: 'swiper-pagination-latest-',
-                            // bulletClass: 'latest-bullet',
-                            // bulletActiveClass: 'erwe',
-                            // el: '.pagination-latest',
-                            // type: 'bullets',
+                        }}
+                        breakpoints={{
+                            768: {
+                              slidesPerView: 4,
+                              spaceBetween: 10
+                            },
+                            320: {
+                                slidesPerView: 2,
+                                spaceBetween: 10,
+                                slidesPerGroup: 2,
+                            }
                         }}
                         >
                             <SwiperSlide><LatestCard/></SwiperSlide>
